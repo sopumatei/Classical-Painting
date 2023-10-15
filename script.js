bd0427f81293a97f3bab1c7bfaa01b56c4963d08
Sopu Matei <sopumatei@gmail.com>
2023-10-15 22:35:30 +0300

    Modify the drawing system
    Now, you can draw more easily by holding down one
    mouse button.

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 // Painting Paper
 const paintingPaper = document.getElementById('painting-paper');
-let pixels = 16;
+let pixels = 64;
 
 const pixelWidth = paintingPaper.offsetWidth / pixels;
 const pixelHeight = paintingPaper.offsetHeight / pixels;
 
+// This part checks if one of the two mouse buttons is held down 
+let mouseDown = 0;
+ window.onmousedown = () => {
+    ++mouseDown;   
+}
+window.onmouseup = () => {
+    --mouseDown;
+}
+
+// This loop creates each pixel inside the painting zone
 for(let i = 1; i <= pixels * pixels; ++i) {
     const pixel = document.createElement('div');
     pixel.style.backgroundColor = "white";
     pixel.style.width = `${pixelWidth}px`;
     pixel.style.height = `${pixelHeight}px`;
 
-    pixel.addEventListener('mousedown', () => {
-        pixel.style.backgroundColor = "black";
+    pixel.addEventListener('mouseenter', () => {
+        if (mouseDown) {
+            pixel.style.backgroundColor = 'black';
+        }
     })
 
     paintingPaper.append(pixel);
